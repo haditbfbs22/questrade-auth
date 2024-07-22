4beaec4fa8f0cac16de25de2724fba5ffb526ae1
haditorabi <haditbfbs22@gmail.com>
2024-07-22 14:04:23 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import questradeLogo from "./assets/Questrade_logo.svg";
 import "./App.css";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  console.log(location.hash);
   return (
     <>
       <div>
@@ -10,8 +13,13 @@ function App() {
         </a>
       </div>
       <h1>An App for Questrade Auth test</h1>
+      <a
+        href={`https://login.questrade.com/oauth2/authorize?client_id=XKRGO8yuCQDGqczbx2onhTu9qEj4oA&response_type=token&redirect_uri=${window.location.href}`}
+        target="_blank"
+      >
+        <button className="btn">Test</button>
+      </a>
     </>
   );
 }
-
 export default App;
